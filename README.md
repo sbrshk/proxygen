d53f9843e49bf15ab1adf306a77047d36f89fbef
sbrshk <BorushkoSA@sibedge.com>
2024-04-19 22:59:28 +0300

    add README.md global and card list configs

---
+++ README.md
@@ -0,0 +1,41 @@
+# proxygen.sh
+A simple bash script to merge separate card images into single ready-to-print pdf file with standard _**Android: Netrunner**_ card sizes (6.2x8.65cm).
+
+#### Depends on imagemagick ^7
+## How to use
+1. Make sure you have a `proxygen.config` file in the same directory with `proxygen.sh`
+2. Place source images into a folder and provide it's relative path to the `SOURCE_DIR` variable in config
+3. If you want to specify numbers of copies for the cards create a _cards config file_ and provide it's relative path to the `CARDS_CONFIG_FILE` variable in config
+4. Specify card sizes and list paddings if needed (though it's highly recommended to keep them the same as in `proxygen.config.example`)
+5. Open terminal, navigate to the folder containing the script and run `/bin/zsh ./proxygen.sh` 
+6. Wait for the script to complete
+7. Find your done and dusted PDF in the output folder, print it, cut the cards and enjoy playing!
+
+Requires `proxygen.config` in the root directory (see `proxygen.config.example`)
+
+Config variables (`!` means required):
+
+- `FILE_EXT` - image file extension (without dot), e.q. "png" (default value is `jpg`)
+- `!SOURCE_DIR` - path to the directory containing source images*
+- `RESULT_DIR` - path to the output directory (default value is `./out`)*
+- `ITEM_WIDTH` - width of the card on the document list in px (recommended value is `732`)**
+- `ITEM_HEIGHT` - height of the card on the document list in px (recommended value is `1018`)**
+- `PADDING_HORIZONTAL` - horizontal padding of the list in px (recommended value is `142`)**
+- `PADDING_VERTICAL` - vertical padding of the list in px (recommended value is `221`)**
+- `CARDS_CONFIG_FILE` - path to the file containing list of cards and their copies needed to be included in the resulting file (see `cards_list.example`). When no config provided every image in the source directory will be used once.***
+
+(*) - all paths are relative to the directory containing `proxygen.sh` file
+
+(**) - for 300 dpi printing
+
+(***) - cards config format example:
+
+`Card name 01.jpg|2`
+
+`Card name 02.jpg|3`
+
+`Card name 03.jpg|1`
+
+`...`
+
+`{card name N}.{file format}|{number of copies of this card}`
--- proxygen.sh
+++ proxygen.sh
@@ -1,23 +1,61 @@
 #!/bin/bash
 
-ITEM_WIDTH=732
-ITEM_HEIGHT=1018
-PADDING_HORIZONTAL=142
-PADDING_VERTICAL=221
+DEFAULT_FILE_EXT=jpg
+DEFAULT_RESULT_DIR=./out
+DEFAULT_ITEM_WIDTH=732
+DEFAULT_ITEM_HEIGHT=1018
+DEFAULT_PADDING_HORIZONTAL=142
+DEFAULT_PADDING_VERTICAL=221
 
-if [  -d "./out" ]; then
-  rm ./out/*
+echo "---------------------------------------------------"
+echo "PROXYGEN SCRIPT STARTED"
+echo "---------------------------------------------------"
+
+echo "STEP 1: reading config..."
+if [ -f ./proxygen.config ]; then
+  echo "STEP 1 SUCCESS: proxygen.config found, using it"
+  . ./proxygen.config
+  FILE_EXT=${FILE_EXT:-$DEFAULT_FILE_EXT}
+  RESULT_DIR=${RESULT_DIR:-$DEFAULT_RESULT_DIR}
+  ITEM_WIDTH=${ITEM_WIDTH:-$DEFAULT_ITEM_WIDTH}
+  ITEM_HEIGHT=${ITEM_HEIGHT:-$DEFAULT_ITEM_HEIGHT}
+  PADDING_HORIZONTAL=${PADDING_HORIZONTAL:-$DEFAULT_PADDING_HORIZONTAL}
+  PADDING_VERTICAL=${PADDING_VERTICAL:-$DEFAULT_PADDING_VERTICAL}
 else
-  mkdir ./out
+  echo "STEP 1 FAILED: please make sure there is a 'proxygen.config' file in the root of current directory"
+  exit 1
+fi
+
+echo "STEP 2: preparing directories..."
+if [  -d "$RESULT_DIR" ]; then
+  rm "$RESULT_DIR"/*
+else
+  mkdir "$RESULT_DIR"
 fi
 mkdir ./tmp
 
-echo "Merging images in current directory to proxy.pdf file..."
-echo "Total images: $(find . -type f -name "*.jpg" | wc -l)"
+if [ -f "$CARDS_CONFIG_FILE" ]; then
+  echo "STEP 3: cards config found, generating card list..."
+  while IFS= read -r LINE
+  do
+      CARD_NAME=$(echo "$LINE" | cut -d '|' -f 1)
+      CARD_COPIES=$(echo "$LINE" | cut -d '|' -f 2)
+      for i in $(seq 1 "$CARD_COPIES"); do
+        echo \""$SOURCE_DIR"/"$CARD_NAME"\" >> ./tmp/cards_list
+      done
+  done < "$CARDS_CONFIG_FILE"
+else
+  echo "STEP 3: cards config not found, all cards in the source directory will be used"
+  echo "$SOURCE_DIR/*.$FILE_EXT" > ./tmp/cards_list
+fi
 
-magick montage *.jpg -tile 3x3 -geometry $((ITEM_WIDTH))x$((ITEM_HEIGHT))+0+0 tmp/output.jpg
+echo "STEP 4: Merging images..."
+magick montage @./tmp/cards_list -tile 3x3 -geometry $((ITEM_WIDTH))x$((ITEM_HEIGHT))+0+0 "tmp/output.$FILE_EXT"
 
-magick convert tmp/*.jpg \
+rm -rf ./tmp/cards_list
+
+echo "STEP 5: Generating PDF file..."
+magick convert "tmp/*.$FILE_EXT" \
   -scale $((ITEM_WIDTH * 3))x"$((ITEM_HEIGHT * 3))" \
   -bordercolor white \
   -border $((PADDING_HORIZONTAL))x$((PADDING_VERTICAL)) \
@@ -34,4 +72,8 @@ magick convert tmp/*.jpg \
 
 rm -rf ./tmp
 
-echo "Done!"
+echo "STEP 6: Done! You can now find your printable PDF file in ${RESULT_DIR}/proxy.pdf"
+
+echo "---------------------------------------------------"
+echo "PROXYGEN SCRIPT FINISHED"
+echo "---------------------------------------------------"
